0bad5cd344380d71ed16c0a3bf7a61e7919d74c1
Nathan Zimmerman <zimmerna@oregonstate.edu>
2017-11-18 23:37:52 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 // Fill in * information to access database
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : '34.226.147.111',
-  user            : 'Test',
-  password        : 'current56pw',
-  database        : 'help_out'
+  host            : 'localhost',
+  user            : '***',
+  password        : '***',
+  database        : '***'
 });
 
 module.exports.pool = pool;
