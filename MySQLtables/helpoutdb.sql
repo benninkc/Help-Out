3b7a1885e1a279a7cfe69709d9e4b1818b199115
tchaddad <tchaddad@gmail.com>
2017-12-01 20:46:44 -0800

    Add more sample events data and more event hosts.

--- MySQLtables/helpoutdb.sql
+++ MySQLtables/helpoutdb.sql
@@ -179,22 +179,34 @@ WHERE (`eventlatitude`> ?MINLAT? And `eventlatitude` < ?MAXLAT?) AND (`eventlong
 # Data for table 'event'
 #
 
-INSERT INTO `event` (`hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES (1, '2017-11-25 00:00:00', 'East bank river clean-up', 'We are going clean up the east bank of the river from OMSI to the Hawthorne bridge.', 'Meet underneath the Hawthorne Bridge on the east side of the river', 45.51251597870104,  -122.66822218894957);
-INSERT INTO `event` (`hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES (5, '2017-12-12 00:00:00', 'Painting benches at Alberta park', 'Seven park benches have been vandalized and need a new coat of paint', 'Meet at the corner of 19th and Killingsworth', 45.56276443144316, -122.64597058296204);
-INSERT INTO `event` (`hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES (1, '2017-12-01 00:00:00', 'Ross Island beach clean-up', 'We will clean up the shores of Ross Island after the heavy rain', 'Clean-up crew departs from Willamette Park boat ramp at 9am sharp', 45.47557036440473, -122.66932725906372);
-# 3 records
+
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(1, 1, '2017-12-27 00:00:00', 'East bank river clean-up', 'We are going clean up the east bank of the river from OMSI to the Hawthorne bridge.', 'Meet underneath the Hawthorne Bridge on the east side of the river', 45.51251597870104, -122.66822218894957);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(2, 5, '2017-12-12 00:00:00', 'Painting benches at Alberta park', 'Seven park benches have been vandalized and need a new coat of paint', 'Meet at the corner of 19th and Killingsworth', 45.56276443144316, -122.64597058296204);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(3, 1, '2017-12-01 00:00:00', 'Ross Island beach clean-up', 'We will clean up the shores of Ross Island after the heavy rain', 'Clean-up crew departs from Willamette Park boat ramp at 9am sharp', 45.47557036440473, -122.66932725906372);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(4, 6, '2017-12-20 00:00:00', 'Potluck in the Park Dessert Service', 'Special theme for Potluck in the Park this month - we are serving desserts', 'Dress warm, ages 16+ welcome', 45.54012756252958, -122.62942671775816);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(5, 7, '2017-12-18 00:00:00', 'Fresh Food Sorting & Repacking at OFB', 'Food bank needs helps organizing donations.', 'Come by and help if you are good with your hands! Ages 16+', 45.580211060077175, -122.6317548751831);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(6, 8, '2017-12-16 00:00:00', 'Sell Christmas Trees ', 'Christmas fundraiser for the Boy Scouts - we need helpers!', 'Serve our visitors cider and cookies as they shop for trees', 45.519338, -122.690682);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(7, 9, '2017-12-13 00:00:00', 'Shop for Seniors in Hollywood', 'Do you like to grocery shop? Would you enjoy helping seniors and people with disabilities maintain their independence? Join us to grocery shop for those who are homebound.', 'Volunteer grocery shoppers receive a client’s grocery list and walk about the store finding each item.', 45.532995774464865, -122.63485550880432);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(8, 10, '2017-12-23 00:00:00', 'Get Books to Children at Childrens Book Bank ', 'Join us at Sellwood public library and lend a hand as we spruce-up community donated books which will later be distributed to low-income preschool children in the Portland area!  Every gently-used donated book is inspected and cleaned by volunteers. ', 'Tasks include wiping covers, taping torn pages, erasing scribbles, covering inscriptions, and reinforcing worn spines.', 45.46768925855662, -122.65277802944183);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(9, 9, '2017-12-28 00:00:00', 'Grocery Shop for Seniors in Beaverton', 'Do you like to grocery shop? Would you enjoy helping seniors and people with disabilities maintain their independence? Join us to grocery shop for those who are homebound.', 'Volunteer grocery shoppers receive a client’s grocery list and walk about the store finding each item.', 45.48544746379536, -122.75590896606445);
+INSERT INTO `event` (`eid`, `hid`, `eventdate`, `eventname`, `eventdescription`, `eventmeetdetails`, `eventlatitude`, `eventlongitude`) VALUES(10, 1, '2017-12-31 00:00:00', 'Eco Crew at Tryon Creek', 'Join a long tradition of community lead park stewardship', 'Volunteer tasks alternate between trail maintenance and invasive plant removal (like mulching, brush clearing, and pulling ivy.) ', 45.436116, -122.676759);
+# 10 records
 
 #
 # Data for table 'host'
 #
 
-INSERT INTO `host` (`hostorg`, `email`, `cid`) VALUES ('Willamette River Keepers', 'cleanrivers@gmail.com', 1);
-INSERT INTO `host` (`hostorg`, `email`, `cid`) VALUES ('Red Cross Portland', 'events@redcross.org', 2);
-INSERT INTO `host` (`hostorg`, `email`, `cid`) VALUES ('Sisters of the Road', 'kitchen@sistersoftheroad.org', 3);
-INSERT INTO `host` (`hostorg`, `email`, `cid`) VALUES ('Portland Youth Soccer', 'kickit@portlandyouth.net', 4);
-INSERT INTO `host` (`hostorg`, `email`, `cid`) VALUES ('Alberta Neighborhood Association', 'goodneighbors@alberta.org', 5);
-# 5 records
-
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(1, 'Willamette River Keepers', 'cleanrivers@gmail.com');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(2, 'Red Cross Portland', 'events@redcross.org');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(3, 'Sisters of the Road', 'kitchen@sistersoftheroad.org');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(4, 'Portland Youth Soccer', 'kickit@portlandyouth.net');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(5, 'Alberta Neighborhood Association', 'goodneighbors@alberta.org');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(6, 'Potluck in the Park', 'potlucks@inpdx.org');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(7, 'Oregon Food Bank', 'pantry@feedoregon.org');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(8, 'Boy Scouts', 'troop45@scouts.or.us');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(9, 'Store to Door', 'shop@forseniors.org');
+INSERT INTO `host` (`hid`, `hostorg`, `email`) VALUES(10, 'Childrens Book Bank', 'books@childrensbank.org');
+# 10 records
 #
 # Data for table 'category'
 #
